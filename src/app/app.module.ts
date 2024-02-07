694f84514f8af4805d41d6115bf36546dab416bd
Noah Torres <atnoah666@gmail.com>
2024-02-07 14:29:20 -0600

    BigUpdate
    Aquí anda el bueno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,33 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimengModule } from './primeng.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    PrimengModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
